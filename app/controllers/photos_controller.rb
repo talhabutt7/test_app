48737f93df2af3b52c630ef2823040e427799a92
talhabutt7 <talhabuttt@gmail.com>
2022-04-19 07:29:50 +0100

    Show method added to Photo + Code refactoring (Use of before filter to fetch the photo from the id that is coming in the params on show, update and delete)

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
-  
+  before_action :set_photo, only: [:show, :destroy, :update]
+
   def index
     @photos = Photo.all
     render json: @photos
@@ -15,7 +16,6 @@ class PhotosController < ApplicationController
   end
 
   def update
-    @photo = Photo.find_by(id: params[:id])
     render json: 'Record not found', status: :unproccessable_entity unless @photo
     if @photo.update(photo_params)
       render json: @photo, status: :ok
@@ -25,12 +25,15 @@ class PhotosController < ApplicationController
   end
 
   def destroy
-    @photo = Photo.find_by(id: params[:id])
     if @photo&.destroy
       render json: 'Record Deleted ', status: :ok
     else
       render json: 'Something went wrong', status: :unproccessable_entity
     end
+  end
+
+  def show
+    render json: @photo, status: :ok
   end
 
   private
@@ -39,5 +42,10 @@ class PhotosController < ApplicationController
     params.permit(:name, :image, :album_id)
   end
 
+  def set_photo
+    unless @photo ||= ::Photo.find_by(id: params[:id])
+      render_not_found(:appointment, I18n.t('render.errors.auth.appointment_id'), :id)
+    end
+  end
 
 end
