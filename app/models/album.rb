93923f120237cd607486a0115168d1668be58e7e
talhabutt7 <talhabuttt@gmail.com>
2022-04-19 09:29:52 +0100

    Validation of presence on Ablum and Photo name

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,5 @@ class Album < ApplicationRecord
   has_many :photos, dependent: :destroy
   accepts_nested_attributes_for :photos, allow_destroy: true
 
+  validates_presence_of :name
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :album
   mount_uploader :image, ImageUploader
+
+  validates_presence_of :name
 end
